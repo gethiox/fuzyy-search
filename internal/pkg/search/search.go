7db91a7e8ce55a88efd8b72226d3008fa5bb5320
Gethiox <gethiox@gmail.com>
2020-09-22 04:37:58 +0200

    Update SearchEngine interface signature

--- internal/pkg/search/search.go
+++ internal/pkg/search/search.go
@@ -1,19 +1,12 @@
 package search
 
-type Book struct {
-	Title  string
-	Author string
-
-	Content *string
-}
-
 type Result struct {
 	Phrase     string // Matched phrase
 	PosS, PosE int    // Position of phrase in book content
 }
 
 type Searcher interface {
-	Search(book Book, phrase string) (Result, error)
+	Search(content *string, phrase string) (Result, error)
 }
 
 func NewSearcher() Searcher {

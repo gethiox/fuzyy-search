71a66700d3d0886f92cb0d223f893069738d8c32
Gethiox <gethiox@gmail.com>
2020-09-22 03:01:29 +0200

    Add findTxtLinkref, update parser implementation to be more robust

--- internal/pkg/dataprovider/parser.go
+++ internal/pkg/dataprovider/parser.go
@@ -5,25 +5,40 @@ import (
 	"regexp"
 )
 
-var queryResponseRegex = regexp.MustCompile(
-	"(?s)" + // flag includes newline character into the scope of dot (.)
-		"<li class=\"booklink\">.*?" +
-		"<a class=\"link\" href=\"(?P<linkref>.*?)\".*?" + // 1
-		"<span class=\"title\">(?P<title>.*?)</span>.*?" + // 2
-		"<span class=\"subtitle\">(?P<subtitle>.*?)</span>.*?" + // 3
-		"</li>",
+var (
+	findBooksRegexStage1 = regexp.MustCompile("(?s)<li class=\"booklink\">.*?</li>")
+	findBooksRegexStage2 = regexp.MustCompile(
+		"(?s)" + // flag includes newline character into the scope of dot (.)
+			"<li class=\"booklink\">.*?" +
+			"<a class=\"link\" href=\"(?P<linkref>.*?)\".*?" + // group 1
+			"<span class=\"title\">(?P<title>.*?)</span>.*?" + // group 2
+			"<span class=\"subtitle\">(?P<subtitle>.*?)</span>.*?" + // group 3
+			"</li>",
+	)
+	findTxtLinkrefRegexStage1 = regexp.MustCompile("(?s)<a href.+?</a>")
+	findTxtLinkrefRegexStage2 = regexp.MustCompile(
+		"(?s)" +
+			"<a href=\"(?P<txtlinkref>\\S*?)\".*?" +
+			"title=\"Download\">Plain Text.*?" +
+			"</a>",
+	)
 )
 
-// parseBooks parses book search query
-func parseBooks(responseBody string) ([]Book, error) {
+// findBooks parses book search query
+func findBooks(responseBody string) ([]Book, error) {
 	var books []Book
 
-	matches := queryResponseRegex.FindAllStringSubmatch(responseBody, -1)
+	matches := findBooksRegexStage1.FindAllString(responseBody, -1)
 	if len(matches) == 0 {
 		return books, errors.New("no results")
 	}
 
-	for _, match := range matches {
+	for _, stage2 := range matches {
+		match := findBooksRegexStage2.FindStringSubmatch(stage2)
+		if match == nil {
+			continue
+		}
+
 		title := match[2]
 		author := match[3]
 		linkref := match[1]
@@ -36,4 +51,22 @@ func parseBooks(responseBody string) ([]Book, error) {
 	}
 
 	return books, nil
+}
+
+// findTxtLinkref parses book entry page
+func findTxtLinkref(responseBody string) (string, error) {
+	matches := findTxtLinkrefRegexStage1.FindAllString(responseBody, -1)
+	if len(matches) == 0 {
+		return "", errors.New("no results")
+	}
+
+	for _, stage2 := range matches {
+		matches := findTxtLinkrefRegexStage2.FindStringSubmatch(stage2)
+		if matches == nil {
+			continue
+		}
+		return matches[1], nil
+	}
+
+	return "", errors.New("no results")
 }

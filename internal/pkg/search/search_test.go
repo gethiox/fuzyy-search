d96817de301d0ebe1d9590920d8d594f52449c3a
Gethiox <gethiox@gmail.com>
2020-09-22 19:43:07 +0200

    Add proper tests for fuzzy-search engine

--- internal/pkg/search/search_test.go
+++ internal/pkg/search/search_test.go
@@ -1,69 +1,211 @@
 package search
 
 import (
-	"io/ioutil"
-	"os"
+	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func loadTestBook(t *testing.T) Book {
-	fd, err := os.OpenFile("search_test_book_content.txt", os.O_RDONLY, 0)
-	if err != nil {
-		t.Fatal("Failed to open book content file: ", err)
-	}
-	data, err := ioutil.ReadAll(fd)
-	if err != nil {
-		t.Fatal("Failed to load book content data: ", err)
+func TestLocalSearcher_SearchSimpleHappyPath(t *testing.T) {
+	type testCase struct {
+		Description    string
+		Searcher       Searcher
+		Content        string
+		Phrase         string
+		expectedResult Result
 	}
 
-	content := string(data)
-	return Book{
-		Title:   "Title",
-		Author:  "Author",
-		Content: &content,
+	var testCases = []testCase{
+		{
+			Description:    "no fuzzing, exact match",
+			Searcher:       NewSearcher(0, 0, 0),
+			Content:        "abcd",
+			Phrase:         "bc",
+			expectedResult: Result{Phrase: "bc", PosS: 1, PosE: 3},
+		}, {
+			Description:    "fuzzing, insertion 1",
+			Searcher:       NewSearcher(1, 0, 0),
+			Content:        "abcde",
+			Phrase:         "bd",
+			expectedResult: Result{Phrase: "bcd", PosS: 1, PosE: 4},
+		}, {
+			Description:    "fuzzing, replacement 1",
+			Searcher:       NewSearcher(0, 1, 0),
+			Content:        "abcde",
+			Phrase:         "bXd",
+			expectedResult: Result{Phrase: "bcd", PosS: 1, PosE: 4},
+		}, {
+			Description:    "fuzzing, deletion 1",
+			Searcher:       NewSearcher(0, 0, 1),
+			Content:        "abcde",
+			Phrase:         "bcXd",
+			expectedResult: Result{Phrase: "bcd", PosS: 1, PosE: 4},
+		},
+	}
+
+	for i, tc := range testCases {
+		name := fmt.Sprintf("%d:%s", i, tc.Description)
+		t.Run(name, func(t *testing.T) {
+			result, err := tc.Searcher.Search(&tc.Content, tc.Phrase)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expectedResult, result)
+		})
 	}
 }
 
-func TestExpectedResult(t *testing.T) {
-	book := loadTestBook(t)
-	phrase := "oh romeo romeo"
-	expectedResult := Result{}
-
-	searcher := NewSearcher()
-
-	result, err := searcher.Search(book, phrase)
-	if err != nil {
-		t.Fatal("Search failed; ", err)
+func TestLocalSearcher_SearchFuzzingInsertion(t *testing.T) {
+	type testCase struct {
+		Description    string
+		Searcher       Searcher
+		Content        string
+		Phrase         string
+		expectedResult Result
 	}
 
-	if expectedResult.Phrase != result.Phrase {
-		t.Fatalf("Phrase does not match, Wanted: \"%s\", Have: \"%s\"", expectedResult.Phrase, result.Phrase)
+	var testCases = []testCase{
+		{
+			Description:    "insertion 1",
+			Searcher:       NewSearcher(1, 0, 0),
+			Content:        "some content to search",
+			Phrase:         "contentto",
+			expectedResult: Result{Phrase: "content to", PosS: 5, PosE: 15},
+		}, {
+			Description:    "insertion 2, two chars in series",
+			Searcher:       NewSearcher(2, 0, 0),
+			Content:        "some content to search",
+			Phrase:         "contento",
+			expectedResult: Result{Phrase: "content to", PosS: 5, PosE: 15},
+		}, {
+			Description:    "insertion 2, two chars in different places",
+			Searcher:       NewSearcher(2, 0, 0),
+			Content:        "some content to search",
+			Phrase:         "cntentto",
+			expectedResult: Result{Phrase: "content to", PosS: 5, PosE: 15},
+		},
 	}
-	if expectedResult.PosS != result.PosS {
-		t.Fatalf("PosS does not match, Wanted: \"%d\", Have: \"%d\"", expectedResult.PosS, result.PosS)
+
+	for i, tc := range testCases {
+		name := fmt.Sprintf("%d:%s", i, tc.Description)
+		t.Run(name, func(t *testing.T) {
+			result, err := tc.Searcher.Search(&tc.Content, tc.Phrase)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expectedResult, result)
+		})
 	}
-	if expectedResult.PosE != result.PosE {
-		t.Fatalf("PosE does not match, Wanted: \"%d\", Have: \"%d\"", expectedResult.PosE, result.PosE)
+}
+func TestLocalSearcher_SearchFuzzingReplacement(t *testing.T) {
+	type testCase struct {
+		Description    string
+		Searcher       Searcher
+		Content        string
+		Phrase         string
+		expectedResult Result
+	}
+
+	var testCases = []testCase{
+		{
+			Description:    "replacement 1",
+			Searcher:       NewSearcher(0, 1, 0),
+			Content:        "some content to search",
+			Phrase:         "content tX",
+			expectedResult: Result{Phrase: "content to", PosS: 5, PosE: 15},
+		}, {
+			Description:    "replacement 2, two chars in series",
+			Searcher:       NewSearcher(0, 2, 0),
+			Content:        "some content to search",
+			Phrase:         "content XX",
+			expectedResult: Result{Phrase: "content to", PosS: 5, PosE: 15},
+		}, {
+			Description:    "replacement 2, two chars in different places",
+			Searcher:       NewSearcher(0, 2, 0),
+			Content:        "some content to search",
+			Phrase:         "conXent oY",
+			expectedResult: Result{Phrase: "content to", PosS: 5, PosE: 15},
+		},
+	}
+
+	for i, tc := range testCases {
+		name := fmt.Sprintf("%d:%s", i, tc.Description)
+		t.Run(name, func(t *testing.T) {
+			result, err := tc.Searcher.Search(&tc.Content, tc.Phrase)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expectedResult, result)
+		})
+	}
+}
+func TestLocalSearcher_SearchFuzzingDeletion(t *testing.T) {
+	type testCase struct {
+		Description    string
+		Searcher       Searcher
+		Content        string
+		Phrase         string
+		expectedResult Result
+	}
+
+	var testCases = []testCase{
+		{
+			Description:    "deletion 1",
+			Searcher:       NewSearcher(0, 0, 1),
+			Content:        "some content to search",
+			Phrase:         "content Wto",
+			expectedResult: Result{Phrase: "content to", PosS: 5, PosE: 15},
+		}, {
+			Description:    "deletion 2, two chars in series",
+			Searcher:       NewSearcher(0, 0, 2),
+			Content:        "some content to search",
+			Phrase:         "content YWto",
+			expectedResult: Result{Phrase: "content to", PosS: 5, PosE: 15},
+		}, {
+			Description:    "deletion 2, two chars in different places",
+			Searcher:       NewSearcher(0, 0, 2),
+			Content:        "some content to search",
+			Phrase:         "contXent Yto",
+			expectedResult: Result{Phrase: "content to", PosS: 5, PosE: 15},
+		},
+	}
+
+	for i, tc := range testCases {
+		name := fmt.Sprintf("%d:%s", i, tc.Description)
+		t.Run(name, func(t *testing.T) {
+			result, err := tc.Searcher.Search(&tc.Content, tc.Phrase)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expectedResult, result)
+		})
 	}
 }
 
-func TestExpectedResultCorrectPosition(t *testing.T) {
-	book := loadTestBook(t)
-	phrase := "oh romeo romeo"
-	expectedMatch := "O Romeo, Romeo"
-
-	searcher := NewSearcher()
-
-	result, err := searcher.Search(book, phrase)
-	if err != nil {
-		t.Fatal("Search failed; ", err)
+func TestLocalSearcher_SearchFuzzingInsertionReplacementDeletion(t *testing.T) {
+	type testCase struct {
+		Description    string
+		Searcher       Searcher
+		Content        string
+		Phrase         string
+		expectedResult Result
 	}
 
-	match := (*book.Content)[result.PosS:result.PosE]
+	var testCases = []testCase{
+		{
+			Description:    "insertion 1, replacement 1, deletion 1",
+			Searcher:       NewSearcher(1, 1, 1),
+			Content:        "some content to search",
+			Phrase:         "cotenR Dto",
+			expectedResult: Result{Phrase: "content to", PosS: 5, PosE: 15},
+		}, {
+			Description:    "insertion 2, replacement 2, deletion 2",
+			Searcher:       NewSearcher(2, 2, 2),
+			Content:        "some content to search",
+			Phrase:         "RtRnDt tDo",
+			expectedResult: Result{Phrase: "content to", PosS: 5, PosE: 15},
+		},
+	}
 
-	if match != expectedMatch {
-		t.Logf("Position range in result is incorrect (%d:%d)", result.PosS, result.PosE)
-		t.Logf("Wanted: \"%s\"", expectedMatch)
-		t.Logf("Have: \"%s\"", match)
+	for i, tc := range testCases {
+		name := fmt.Sprintf("%d:%s", i, tc.Description)
+		t.Run(name, func(t *testing.T) {
+			result, err := tc.Searcher.Search(&tc.Content, tc.Phrase)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expectedResult, result)
+		})
 	}
 }
